a6949b41bcf67ca5eeb03bb7a5b61d8bdec196cb
IA-PieroCV <piero.casusol@intautomation.net>
2024-11-28 00:10:50 -0500

    feat: data types and variables, challenge 1

---
+++ variables.py
@@ -0,0 +1,15 @@
+notas = 16 
+notas = notas + 15
+notas = notas + 14
+notas = notas + 19
+print(notas/4)
+
+valor_nulo = None
+
+print(valor_nulo is None)
+
+print("El promedio de las notas es", notas/4, "xd pasamos")
+
+print("El promedio de las notas es {0} xd pasamos".format(notas/4))
+
+print(f"El promedio de las notas es {notas/4} xd pasamos")
